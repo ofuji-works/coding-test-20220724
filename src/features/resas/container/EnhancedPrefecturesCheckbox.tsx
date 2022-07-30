41abd75f61be60463a9c665eb8cb7c876a84240a
ofuji-works <ofuji.works@gmail.com>
2022-07-31 00:05:03 +0900

    都道府県チェックボックス一覧Component構造変更

--- src/features/resas/container/EnhancedPrefecturesCheckbox.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react"
-
-import { PrefecturesCheckbox } from "../component"
-import { usePrefectures } from "../hook"
-
-export const EnhancedPrefecturesCheckbox: FC = () => {
-  const { prefectures } = usePrefectures()
-  return <PrefecturesCheckbox prefectures={prefectures} />
-}
--- src/features/resas/container/index.ts
+++ src/features/resas/container/index.ts
@@ -1,2 +1,1 @@
 export * from "./EnhancedResasDataAnalyze"
-export * from "./EnhancedPrefecturesCheckbox"
