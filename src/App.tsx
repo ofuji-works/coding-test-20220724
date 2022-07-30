19bf08243b349492d35a2343860e9d1706c3bea3
ofuji-works <ofuji.works@gmail.com>
2022-07-31 00:07:10 +0900

    Layout反映

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Layout } from "components"
 import { EnhancedResasDataAnalyze } from "features/resas"
 
 function App() {
-  return <EnhancedResasDataAnalyze />
+  return (
+    <Layout title="都道府県別の総人口推移グラフ">
+      <EnhancedResasDataAnalyze />
+    </Layout>
+  )
 }
 
 export default App
